301ba7c84f6495a9c602ddadc38cb61fc8c75f98
Luka Dekanozishvili <luka.dekanozishvili1@gmail.com>
2024-05-09 22:14:10 +0200

    improved asm formatting

--- output.c
+++ output.c
@@ -8,143 +8,143 @@
 
 void fprint_arg(uint16_t num, FILE* asm_file) {
     if (num >= REG0 && num <= REG7) {
-        fprintf(asm_file, "REG%d\t", num - REG0);
+        fprintf(asm_file, "REG_%d ", num - REG0);
     } else {
-        fprintf(asm_file, "%05d\t", num);
+        fprintf(asm_file, "%05d ", num);
     }
 }
 
 void fprint_asm_line(FILE* fp) {
-    fprintf(fp, "%05zu: ", PC);
+    fprintf(fp, "%05zu ", PC);
     uint16_t opcode = buffer[PC++];
 
     switch(opcode) {
     case OP_HALT:
-        fprintf(fp, "%s\t\t-----------------------", op_arr[opcode]);
+        fprintf(fp, "%s-----------------------", op_arr[opcode]);
         fprintf(fp, "\n");
         break;
     case OP_SET:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_PUSH:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_POP:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_EQ:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_GT:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_JMP:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_JT:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_JF:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_ADD:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_MULT:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_MOD:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_AND:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_OR:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_NOT:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_RMEM:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_WMEM:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_CALL:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_RET:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprintf(fp, "\n");
         break;
     case OP_OUT:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         char ch = buffer[PC-1];
-        fprintf(fp, "\t\t\t(%c)\n", isprint(ch) ? ch : '.');
+        fprintf(fp, "\t(%c)\n", isprint(ch) ? ch : '.');
         break;
     case OP_IN:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprint_arg(buffer[PC++], fp);
         fprintf(fp, "\n");
         break;
     case OP_NOOP:
-        fprintf(fp, "%s\t\t", op_arr[opcode]);
+        fprintf(fp, "%s ", op_arr[opcode]);
         fprintf(fp, "\n");
         break;
     default:
